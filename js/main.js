7866ee787dce26561906eb10c4ffe8b28977a0f6
Francesco <francesco.ferruccio88@gmail.com>
2020-04-04 11:40:10 +0200

    generazione random quadratini rossi

--- js/main.js
+++ js/main.js
@@ -7,6 +7,23 @@ $(document).ready(function() {
     griglia.html(prevContent + "<div class='box'></div>");
   }
 
+  // creo un ciclo che assegna in maniera random la classe red ai quadratini
+  for (var j = 15; j > 0; j--) {
+    // genero un numero casuale tra 1 e 64
+    var index = Math.floor(Math.random() * 64 + 1);
+    console.log("numero random " + j + ": " + index);
+    // seleziono il div con l'indice random corrispondente
+    var divSelected = $("#griglia div").eq(index);
+    // controllo se il div selezionato ha gia la classe red
+    // se ce l'ha incremento il contatore del ciclo per ripetere il ciclo
+    // se non ce l'ha gliela assegno
+    if (divSelected.hasClass("red")) {
+      j++;
+    } else {
+      $("#griglia div").eq(index).addClass("red");
+    }
+  }
+
 
 
 
